53b02b4fd3d447702357e0e7f9ea35c20416ae87
abdoulfataho <yamyaogofataho@gmail.com>
2024-07-23 09:24:14 -0400

    final work

---
+++ src/exampleTreemapTwo.java
@@ -0,0 +1,15 @@
+import java.util.TreeMap;
+
+public class exampleTreemapTwo {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> numbers = new TreeMap<>();
+        numbers.put("First", 1);
+        numbers.put("Second", 2);
+        numbers.put("Third", 3);
+        System.out.println("TreeMap: " + numbers);
+
+        // Using the firstKey() method
+        String firstKey = numbers.firstKey();
+        System.out.println("First Key: " + firstKey);
+    }
+}
